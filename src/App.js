953be7e67ae1751defc77dda879605cff883874a
Irena Plohl <irenap@portia-shop.com>
2026-01-28 16:02:39 +0100

    add todo  ( ne izpise error )

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import "./styles/general/main.scss";
 import Heading from "./Components/Heading/Heading";
-import Input from "./Components/Input/Input";
-import Button from "./Components/Button/Button";
-import Form from "./Components/Form/Form";
-import Footer from "./Components/Foother/Foother";
+import Footer from "./Components/Footer/Footer";
+import Todo from "./Components/Todo/Todo";
 
 export function App() {
     return (
         <div className="container">
             <div className="row">
-                <Heading title="Daily Todo's" />
-                <Form>
-                    <Input title="Add a task" id="task" name="task"/>
-                    <Button title="Add Task"/>
-                </Form>
-                <Footer title="xxxxxxx"/>
+                <Heading title="Daily Todo's"/>
+                <Todo/>
+                <Footer title="Coders Lab"/>
             </div>
         </div>
     );
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from "./_foother.module.scss";
+import styles from "./_footer.module.scss";
 
 
 const Footer = ({title}) => {
