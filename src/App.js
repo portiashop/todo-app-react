af099535bc6ec0de261129e21c25d94b7678884c
Irena Plohl <irenap@portia-shop.com>
2026-01-28 20:52:26 +0100

    add logic for add task

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import "./styles/general/main.scss";
 import Heading from "./Components/Heading/Heading";
 import Footer from "./Components/Footer/Footer";
 import Todo from "./Components/Todo/Todo";
-
 export function App() {
     return (
         <div className="container">
             <div className="row">
-                <Heading title="Daily Todo's"/>
+                <Heading title="Daily Todo's" />
                 <Todo/>
-                <Footer title="Coders Lab"/>
+                <Footer title="vsb.uidev"/>
             </div>
         </div>
     );
