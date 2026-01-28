2f49a8ad2924c73ab45f38b7a6d6820c59ee48dd
Irena Plohl <irenap@portia-shop.com>
2026-01-28 14:33:28 +0100

    add form and foother

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import "./styles/general/main.scss";
 import Heading from "./Components/Heading/Heading";
 import Input from "./Components/Input/Input";
 import Button from "./Components/Button/Button";
+import Form from "./Components/Form/Form";
+import Footer from "./Components/Foother/Foother";
+
 export function App() {
     return (
         <div className="container">
-            <Heading title="Daily Todo's" />
-            <form>
-                <Input title="Add a task" id="task" name="task"/>
-                <Button title="Add Task"/>
-            </form>
-            <p>Edit <code>src/App.tsx</code> to get started!</p>
+            <div className="row">
+                <Heading title="Daily Todo's" />
+                <Form>
+                    <Input title="Add a task" id="task" name="task"/>
+                    <Button title="Add Task"/>
+                </Form>
+                <Footer title="xxxxxxx"/>
+            </div>
         </div>
     );
 }
