9ecae6b333db3ad9566b0b7d537d4de32b4088cf
Irena Plohl <irenap@portia-shop.com>
2026-01-28 12:12:57 +0100

    add container

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Heading from "./Components/Heading/Heading";
 export function App() {
     return (
         <div className="container">
-            <Heading title="Daily Todo's" />
+            <Heading title=" Haire car api" />
             <p>Edit <code>src/App.tsx</code> to get started!</p>
         </div>
     );
